1b662d860f70c7950b6ca67d9cec0d06366df53f
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-29 11:24:34 -0700

    add more specific descriptors for country_of_origin and content_body attrs.

--- db/migrate/20200529170323_create_products.rb
+++ db/migrate/20200529170323_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
     create_table :products do |t|
       t.string :name
       t.integer :cost
-      t.string :country
+      t.string :country_of_origin
 
       t.timestamps
     end
--- db/migrate/20200529170339_create_reviews.rb
+++ db/migrate/20200529170339_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
       t.string :authors
-      t.string :content
+      t.string :content_body
       t.integer :rating
 
       t.timestamps
