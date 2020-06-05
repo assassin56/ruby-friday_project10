43f0f7a0eff5b74fdca8c66eba923e205b7552fd
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-05 15:27:31 -0700

    add new test for adding a review. Test fails.:

---
+++ spec/features/add_review_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe " the process in adding a review" do
+  it "adds a review to the product" do
+    new_product = Product.create!({:name => "Doom Guy", 
+    :cost => "666", 
+    :country => "United Aerospace Corporation"})
+    visit products_path
+    click_link 'Doom Guy'
+    click_link 'Add a review'
+    fill_in 'Author', :with => "John Romero"
+    fill_in 'Content body', :with => "I was not very happy with this product. All it did was rip and tear my carpet. I am a very displeased customer and I demand a refund"
+    fill_in 'Rating', :with => 2
+    click_on 'Create Review'
+    expect(page).to have_content 'This should fail'
+  end
+end
