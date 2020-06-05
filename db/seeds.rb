0f416fd6784949fe4c3237f33b22c93cfaf51f80
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-05 14:13:02 -0700

    add seeds for simulation of database interaction.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.destroy_all
+
+50.times do |product|
+  product = Product.create!(
+    name: Faker::Food.ingredient, 
+    cost: Faker::Number.within(range: 10..30),
+    country: Faker::WorldCup.team)
+  
+  10.times do |review|
+    Review.create!(author: Faker::FunnyName.two_word_name, 
+    content_body: Faker::Lorem.characters(number: 55),
+    rating: rand(1..5),
+    product_id: product.id
+    )
+  end
+end
