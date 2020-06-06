0a75f3c134ad658b0798d5daa362d45a8eb016e6
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-05 17:45:24 -0700

    add current_user method to ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :expection
+  helper_method :current_user
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(sessions[:user_])
+    end
+  end
 end
