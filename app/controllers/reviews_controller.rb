116c813e26c63333caed2a31e6bb340a4548e9fa
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-14 15:38:14 -0700

    delete unnecessary comma causing an error on line 3.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :authorize, :except => [:show]
-  before_action :only, => [:edit, :destroy] do
+  before_action :only => [:edit, :destroy] do
     redirect_to signin_path unless current_user && current_user.admin
     flash[:notice] = "Sorry, that command is only for admins. Please sign in as an admin to continue."
   end
