1625edda1b7aad46afff36cc3e8e0344901c91b6
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-17 10:45:43 -0700

    add before :each block to create a new user for delete_product_pages tests.

--- spec/features/delete_product_pages_spec.rb
+++ spec/features/delete_product_pages_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 describe "the process of deleting a product" do
-  it "deletes a product" do
-    new_product = Product.create!({:name => "Cyber Demon", 
-    :cost => 333, 
-    :country => "Hell"})
-    # fill_in 'Email', :with => "codrocks.cruz3@gmail.com"
-    # fill_in 'Password', :with => "password"
-    # click_on 'Sign In'
-    visit products_path
-    click_link "Cyber Demon"
-    click_link "Delete"
-    expect(page).to have_no_content "Cyber Demon"
-  end
+  
+  # before :each do
+  #   new_user = User.create!({:email => "codrocks.cruz3@gmail.com", :password => "password", :password_confrimation => "password", :admin => true})
+
+  #   it "deletes a product" do
+  #     new_product = Product.create!({:name => "Cyber Demon", 
+  #     :cost => 333, 
+  #     :country => "Hell"})
+  #     # fill_in 'Email', :with => "codrocks.cruz3@gmail.com"
+  #     # fill_in 'Password', :with => "password"
+  #     # click_on 'Sign In'
+  #     visit products_path
+  #     click_link "Cyber Demon"
+  #     click_link "Delete"
+  #     expect(page).to have_no_content "Cyber Demon"
+  #   end
+  # end
 end
