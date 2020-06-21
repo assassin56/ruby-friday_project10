89b06aca2bed281bed71f7ec0f680ff4ef88bc7c
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-21 16:40:19 -0700

    edit reviews views files to accomodate new _review_form.html.erb.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :authorize, :except => [:show]
   before_action :only => [:edit, :destroy] do
-    redirect_to signin_path unless current_user && current_user.admin
+    redirect_to signin_path unless current_user || current_user.admin
     flash[:notice] = "Sorry, that command is only for admins. Please sign in as an admin to continue."
   end
 
