fccc28a0e81b5a7fc1abf4a50ec09a07564000a2
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-21 15:12:38 -0700

    add link for to create a new product in application.html.erb. As well, add before each block for a new_user for add_product_pages_spec.rb.

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -4,28 +4,28 @@ describe "the process in adding a product" do
 
   before :each do
     new_user = User.create!({:email => "codrocks.cruz3@gmail.com", :password => "password", :password_confirmation => "password", :admin => true})
-
-    it "adds a new product" do
-      # visit signin_path
-      # fill_in 'Email', :with => "codrocks.cruz3@gmail.com"
-      # fill_in 'Password', :with => "password"
-      # click_on 'Sign In'
-      click_link 'Create new product'
-      fill_in 'Name', :with => "Pizza"
-      fill_in 'Cost', :with => "14"
-      fill_in 'Country', :with => "United States"
-      click_on 'Create Product'
-      expect(page).to have_content "Product successfully added!"
-    end
-
-    # it "throws and error when a product property is not provided" do
-    #   # visit signin_path
-    #   # fill_in 'Email', :with => "codrocks.cruz3@gmail.com"
-    #   # fill_in 'Password', :with => "password"
-    #   # click_on 'Sign In'
-    #   # visit new_product_path
-    #   click_on 'Create Product'
-    #   expect(page).to have_content "Name can't be blank"
-    # end
   end
+
+  it "adds a new product" do
+    visit signin_path
+    fill_in 'Email', :with => "codrocks.cruz3@gmail.com"
+    fill_in 'Password', :with => "password"
+    click_on 'Sign In'
+    click_link 'Create new product'
+    fill_in 'Name', :with => "Pizza"
+    fill_in 'Cost', :with => "14"
+    fill_in 'Country', :with => "United States"
+    click_on 'Create Product'
+    expect(page).to have_content "Product successfully added!"
+  end
+
+  # it "throws and error when a product property is not provided" do
+  #   # visit signin_path
+  #   # fill_in 'Email', :with => "codrocks.cruz3@gmail.com"
+  #   # fill_in 'Password', :with => "password"
+  #   # click_on 'Sign In'
+  #   # visit new_product_path
+  #   click_on 'Create Product'
+  #   expect(page).to have_content "Name can't be blank"
+  # end
 end
