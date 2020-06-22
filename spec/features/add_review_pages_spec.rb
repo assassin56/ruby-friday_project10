8c77b1118fdd183f7a62e5dffb1ac0038d9f7913
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-21 17:13:43 -0700

    add a review test now passes.

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -2,24 +2,25 @@ require 'rails_helper'
 
 describe "the process in adding a review" do
 
-  # before :each do
-  #   new_user = User.create!({:email => "codrocks.cruz3@gmail.com", :password => "passsword", :password_confirmation => "password", :admin => true})
+  before :each do
+    new_user = User.create!({:email => "codrocks.cruz3@gmail.com", :password => "password", :password_confirmation => "password", :admin => true})
+  end
 
-  #   it "adds a review to the product" do
-  #     new_product = Product.create!({:name => "Doom Guy", 
-  #     :cost => "666", 
-  #     :country => "United Aerospace Corporation"})
-  #     # fill_in 'Email', :with => "codrocks.cruz3@gmail.com"
-  #     # fill_in 'Password', :with => "password"
-  #     # click_on 'Sign In'
-  #     visit products_path
-  #     click_link 'Doom Guy'
-  #     click_link 'Add a review'
-  #     fill_in 'Author', :with => "John Romero"
-  #     fill_in 'Content body', :with => "I was not very happy with this product. All it did was rip and tear my carpet. I am a very displeased customer and I demand a refund"
-  #     fill_in 'Rating', :with => 2
-  #     click_on 'Create Review'
-  #     expect(page).to have_content 'John Romero'
-  #   end
-  # end
+  it "adds a review to the product" do
+    new_product = Product.create!({:name => "Doom Guy", 
+    :cost => "666", 
+    :country => "United Aerospace Corporation"})
+    visit signin_path
+    fill_in 'Email', :with => "codrocks.cruz3@gmail.com"
+    fill_in 'Password', :with => "password"
+    click_on 'Sign in'
+    visit products_path
+    click_link 'Doom Guy', :match => :first
+    click_link 'Add a review'
+    fill_in 'Author', :with => "John Romero"
+    fill_in 'Content body', :with => "I was not very happy with this product. All it did was rip and tear my carpet. I am a very displeased customer and I demand a refund"
+    fill_in 'Rating', :with => 2
+    click_on 'Create Review'
+    expect(page).to have_content 'John Romero'
+  end
 end
