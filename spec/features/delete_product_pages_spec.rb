1ae9130348a13168de349302c1d71bdbef65390e
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-21 17:16:57 -0700

    test to delete a product now passes.

--- spec/features/delete_product_pages_spec.rb
+++ spec/features/delete_product_pages_spec.rb
@@ -2,20 +2,21 @@ require 'rails_helper'
 
 describe "the process of deleting a product" do
   
-  # before :each do
-  #   new_user = User.create!({:email => "codrocks.cruz3@gmail.com", :password => "password", :password_confrimation => "password", :admin => true})
+  before :each do
+    new_user = User.create!({:email => "codrocks.cruz3@gmail.com", :password => "password", :password_confirmation => "password", :admin => true})
+  end
 
-  #   it "deletes a product" do
-  #     new_product = Product.create!({:name => "Cyber Demon", 
-  #     :cost => 333, 
-  #     :country => "Hell"})
-  #     # fill_in 'Email', :with => "codrocks.cruz3@gmail.com"
-  #     # fill_in 'Password', :with => "password"
-  #     # click_on 'Sign In'
-  #     visit products_path
-  #     click_link "Cyber Demon"
-  #     click_link "Delete"
-  #     expect(page).to have_no_content "Cyber Demon"
-  #   end
-  # end
+  it "deletes a product" do
+    new_product = Product.create!({:name => "Cyber Demon", 
+    :cost => 333, 
+    :country => "Hell"})
+    visit signin_path
+    fill_in 'Email', :with => "codrocks.cruz3@gmail.com"
+    fill_in 'Password', :with => "password"
+    click_on 'Sign in'
+    visit products_path
+    click_link "Cyber Demon", :match => :first
+    click_link "Delete"
+    expect(page).to have_no_content "Cyber Demon"
+  end
 end
